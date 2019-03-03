0f9ff99647e4f0646a2f284cc85712ad1acec9a1
ffastym <yuriy.matviyuk22@gmail.com>
2019-03-03 11:15:50 +0200

    add babel to dependencies

--- precache-manifest.78ae54a351f149a0f6acd8a3f95fcb73.js
+++ precache-manifest.78ae54a351f149a0f6acd8a3f95fcb73.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "1a7e007ffa621ec037bb",
-    "url": "/static/js/main.1a7e007f.chunk.js"
+    "revision": "a2e5a4bf4a321f264ff8",
+    "url": "/static/js/main.a2e5a4bf.chunk.js"
   },
   {
     "revision": "2257f913953d59ad1d9d",
     "url": "/static/js/1.2257f913.chunk.js"
   },
   {
-    "revision": "1a7e007ffa621ec037bb",
+    "revision": "a2e5a4bf4a321f264ff8",
     "url": "/static/css/main.f85a90c1.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.fd8830d1.chunk.css"
   },
   {
-    "revision": "ab0c24f951a4aa952b8536a6dd9bd45b",
+    "revision": "2006f560b71b1beea85e19a4534f51df",
     "url": "/index.html"
   }
 ];
