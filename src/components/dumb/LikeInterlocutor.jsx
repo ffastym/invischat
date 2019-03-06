a08e95a4cf211a49aea687f49b85dfc8127e09c3
ffastym <yuriy.matviyuk22@gmail.com>
2019-03-06 16:33:09 +0200

    add favorite list

---
+++ src/components/dumb/LikeInterlocutor.jsx
@@ -0,0 +1,20 @@
+/**
+ * @author Yuriy Matviyuk
+ */
+import React from 'react'
+
+/**
+ * LikeInterlocutor component
+ *
+ * @param props
+
+ * @returns {*}
+ * @constructor
+ */
+const LikeInterlocutor = (props) => {
+    return (
+        <div></div>
+    )
+};
+
+export default LikeInterlocutor
